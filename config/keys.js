674e1a3cd91dc60bc9971d5ab9a6675a2ea5e3f2
codemaster6152 <punctualityman@outlook.com>
2019-01-17 10:24:50 +1000

    added config

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://codemaster:dkanehahffk6152@ds053216.mlab.com:53216/devconnect'
+  mongoURI: 'mongodb://codemaster:dkanehahffk6152@ds053216.mlab.com:53216/devconnect',
+  secretOrKey: 'secret'
 }
